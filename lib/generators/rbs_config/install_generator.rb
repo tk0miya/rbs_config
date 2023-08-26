020e1e1423361f934e35005e7cf2f5d5e81d4300
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-08-26 15:41:22 +0900

    Allow to switch Rails mode via Rake task

--- lib/generators/rbs_config/install_generator.rb
+++ lib/generators/rbs_config/install_generator.rb
@@ -9,11 +9,17 @@ module RbsConfig
         begin
           require 'rbs_config/rake_task'
           RbsConfig::RakeTask.new do |task|
-            # The class name of configuration object.
+            # The type of configuration (default: :config)
+            # task.type = :config | :rails
+
+            # The class name of configuration object (for :config type)
             # task.class_name = "Settings"
 
-            # The files to be loaded.
+            # The files to be loaded (for :config type)
             # task.files = [Pathname(Rails.root / "config/settings.yml")]
+
+            # The mapping of configuration (for :rails type)
+            # task.mapping = { foo: Rails.application.config_for(:foo) }
           end
         rescue LoadError
           # failed to load rbs_config. Skip to load rbs_config tasks.
